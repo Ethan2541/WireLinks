d30d760cb3358b391e0574ca52cde7413c3115ac
Ethan2541 <eluong41@gmail.com>
2022-12-03 16:06:54 +0100

    Clean

--- test2.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 65432))
-print(s.recv(1024))
-s.send(b"Bonjour Serveur 65432, nous sommes donc dans la phase de transfert")
-s.send(b"Serveur 65432, je vous dis au revoir")
-s.close()
